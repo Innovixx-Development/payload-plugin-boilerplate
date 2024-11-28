a494e6c733db1770f0df8467ca615003453da734
Kalon Robson <kalon.robson@innovixx.co.uk>
2024-11-28 20:42:36 +0000

    chore: update TypeScript imports and configuration for module support

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import deepmerge from 'deepmerge'
-import type { Plugin } from 'payload/config'
 
 import type { PluginConfig } from './types'
+import { Plugin } from 'payload'
 
 export default (pluginConfig: PluginConfig): Plugin =>
   config => {
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
-import type { Config } from 'payload/config'
+import { Config } from "payload";
+
 export interface PluginConfig {
   overwrites?: Partial<Config>
 }
