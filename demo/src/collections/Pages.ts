dffaa050f6635b9eb0a54930d4f02f1ec3ba4ae8
Kalon Robson <kalon.robson@innovixx.co.uk>
2024-11-28 20:24:09 +0000

    chore: update demo to payload v3

--- demo/src/collections/Pages.ts
+++ demo/src/collections/Pages.ts
@@ -1,20 +1,17 @@
-import type { CollectionConfig } from 'payload/types'
+// eslint-disable-next-line import/no-relative-packages
+import type { CollectionConfig } from 'payload'
+
+// import { colorPickerField } from '../../../dist'
 
 const Pages: CollectionConfig = {
   slug: 'pages',
-  admin: {
-    useAsTitle: 'title',
-  },
   fields: [
     {
       name: 'title',
       type: 'text',
       required: true,
     },
-    {
-      name: 'excerpt',
-      type: 'text',
-    },
+
     {
       name: 'slug',
       type: 'text',
@@ -27,10 +24,32 @@ const Pages: CollectionConfig = {
         position: 'sidebar',
       },
     },
+    // colorPickerField({
+    //   name: 'primaryColor',
+    //   label: 'Primary Color',
+    //   admin: {
+    //     description: 'Pick a color for this page',
+    //   },
+    //   colors: ['#ff0000', '#00ff00', '#0000ff', '#000000', '#ffffff', '#ff00ff'],
+    // }),
+    {
+      name: 'excerpt',
+      type: 'text',
+    },
     {
       name: 'date',
       type: 'date',
     },
+    // colorPickerField({
+    //   name: 'secondaryColor',
+    //   label: 'Secondary Color',
+
+    //   admin: {
+    //     position: 'sidebar',
+    //     description: 'Pick a secondary color for this page',
+    //     rtl: true,
+    //   },
+    // }),
   ],
 }
 
